643ca42db93f2f731adc39265971805b13182170
Samuel Morales <samuelmj1999@gamil.com>
2022-02-21 16:28:44 -0400

    i fixed the board counter

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 let cpuCount = 0;
 let userCount = 0;
+let reload = document.querySelector('.reload').addEventListener('click', _ =>{location.reload()})
+document.getElementById('cpu-counter').innerHTML=cpuCount;
+document.getElementById('user-counter').innerHTML=userCount;
 // let arrayCard = ['piedra', 'papel', 'tijera'];
 // let random = Math.floor(Math.random() * arrayCard.length);
 // let cpu = arrayCard[random];
@@ -44,19 +47,21 @@ function play (cpuSelection, user){
                 // let piedraUser = document.querySelector('.card-cpu');
                 // piedraUser.classList.toggle('piedra');
                 console.log('empate')
+                document.getElementById('marcador').innerHTML='Tie';
             break;
             case 'papel':
                 // let papelUser = document.querySelector('.card-cpu');
                 // papelUser.classList.toggle('papel');
                 cpuCount++;
                 console.log('perdiste')
+                document.getElementById('marcador').innerHTML='You Lose';
             break;
             case 'tijera':
                 // let tijeraUser = document.querySelector('.card-cpu');
                 // tijeraUser.classList.toggle('tijera');
                 userCount++;
                 console.log('haz ganado' + userCount)
-
+                document.getElementById('marcador').innerHTML='You Won';
             break;
         }
     }
@@ -66,18 +71,21 @@ function play (cpuSelection, user){
                 // let papelUser = document.querySelector('.card-cpu');
                 // papelUser.classList.toggle('papel');
                 console.log('empate');
+                document.getElementById('marcador').innerHTML='Tie';
             break;
             case 'tijera':
                 // let tijeraUser = document.querySelector('.card-cpu');
                 // tijeraUser.classList.toggle('tijera');
                 cpuCount++;
                 console.log('perdiste');
+                document.getElementById('marcador').innerHTML='You Lose';
             break;
             case 'piedra':
                 // let piedraUser = document.querySelector('.card-cpu');
                 // piedraUser.classList.toggle('piedra');
                 userCount++;
                 console.log('haz ganado' + userCount);
+                document.getElementById('marcador').innerHTML='You Won';
             break;
         }
     }
@@ -87,22 +95,30 @@ function play (cpuSelection, user){
                 // let tijeraUser = document.querySelector('.card-cpu');
                 // tijeraUser.classList.toggle('tijera');
                 console.log('empate');
+                document.getElementById('marcador').innerHTML='Tie';
             break;
             case 'piedra':
                 // let piedraUser = document.querySelector('.card-cpu');
                 // piedraUser.classList.toggle('piedra');
                 cpuCount++;
                 console.log('perdiste');
+                document.getElementById('marcador').innerHTML='You Lose';
+
             break;
             case 'papel':
                 // let papelUser = document.querySelector('.card-cpu');
                 // papelUser.classList.toggle('papel');
                 userCount++;
                 console.log('haz ganado' + userCount);
+                document.getElementById('marcador').innerHTML='You Won';
+
             break;
         }
     }
     console.log(`Cpu: ${cpuCount} User: ${userCount}`)
+    document.getElementById('cpu-counter').innerHTML=cpuCount;
+    document.getElementById('user-counter').innerHTML=userCount;
+
 }
 
 function cpu(){
